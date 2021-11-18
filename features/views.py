c69abe2f1df50397040790e70c3d86ff3b72adef
Kaushal Kumar <kkum9480@uni.sydney.edu.au>
2021-11-18 14:13:37 +1100

    New Homepage

--- features/views.py
+++ features/views.py
@@ -8,20 +8,10 @@ import json
 
 from django.http import HttpResponseRedirect
 
-# def secure_required(view_func):
-#     def _wrapped_view_func(request, *args, **kwargs):
-#         if request and not request.is_secure():
-#             request_url = request.build_absolute_uri(request.get_full_path())
-#             secure_url = request_url.replace('http://', 'https://')
-#             return HttpResponseRedirect(secure_url)
-#         return view_func(request, *args, **kwargs)
-#     return _wrapped_view_func
-
 # Create your views here.
-# @secure_required
 def index(request):
-    data = {'APP' : 'DIGITAL HEALTH APP'}
-    return JsonResponse(data)
+    # data = {'APP' : 'DIGITAL HEALTH APP'}
+    return render(request, 'home.html')
 
 # Create Health Professional:
 def create_health_professional(request):
